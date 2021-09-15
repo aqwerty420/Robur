49fc493f85ffaa230b95dfb4db30ac56b6a3474c
aqwerty420 <57297980+aqwerty420@users.noreply.github.com>
2021-09-15 11:55:40 +0200

    PoncheJinx (WIP)

---
+++ output/PoncheJinx.lua
@@ -0,0 +1,432 @@
+-- Lua Library inline imports
+function __TS__ArrayIncludes(self, searchElement, fromIndex)
+    if fromIndex == nil then
+        fromIndex = 0
+    end
+    local len = #self
+    local k = fromIndex
+    if fromIndex < 0 then
+        k = len + fromIndex
+    end
+    if k < 0 then
+        k = 0
+    end
+    for i = k, len do
+        if self[i + 1] == searchElement then
+            return true
+        end
+    end
+    return false
+end
+
+function __TS__ArrayPush(arr, ...)
+    local items = {...}
+    for ____, item in ipairs(items) do
+        arr[#arr + 1] = item
+    end
+    return #arr
+end
+
+local ____exports = {}
+local ObjectManager, Game, SpellSlots, Orbwalker, Menu, TargetSelector, fishbonesStack, fishbonesRange, isFishBones, powPowRange, Q, wInput, W, eInput, E, GetValidNearbyHeroes, OnGapclose, OnHeroImmobilized, OnDraw, GetAoeCount, ShouldSwap, tryQ, tryW, tryE, Combo, Harass, HasStatik, LastHit, UpdateStats, OnTick
+function GetValidNearbyHeroes(team)
+    local heroes = {}
+    for key, obj in pairs(
+        ObjectManager.GetNearby(team, "heroes")
+    ) do
+        if TargetSelector:IsValidTarget(obj) then
+            __TS__ArrayPush(heroes, obj.AsHero)
+        end
+    end
+    return heroes
+end
+function OnGapclose(source, dash)
+    if (((((Menu.Get("eOnGapclose") and source.IsHero) and source.IsEnemy) and source.IsValid) and Menu.Get("eOnGap" .. source.CharName)) and E:IsReady()) and (E:GetManaCost() <= Player.Mana) then
+        local paths = dash:GetPaths()
+        local endPos = paths[#paths].EndPos
+        if endPos:Distance(Player.Position) <= eInput.Range then
+            E:Cast(endPos)
+            return
+        end
+    end
+end
+function OnHeroImmobilized(source, endTime, isStasis)
+    if (((((Menu.Get("eOnCC") and source.IsEnemy) and source.IsValid) and E:IsReady()) and (E:GetManaCost() <= Player.Mana)) and (Menu.Get("eCCDuration") <= endTime)) and E:CanCast(source) then
+        E:Cast(source)
+        return
+    end
+end
+function OnDraw()
+end
+function GetAoeCount(target, enemies)
+    local count = 0
+    local radius = Menu.Get("aoeRadius")
+    do
+        local i = 0
+        while i < #enemies do
+            if (target.Name ~= enemies[i + 1].Name) and (target.Position:Distance(enemies[i + 1].Position) < radius) then
+                count = count + 1
+            end
+            i = i + 1
+        end
+    end
+    return count
+end
+function ShouldSwap(target, enemies)
+    local distanceTarget = Player.Position:Distance(target.Position)
+    local isInFishBonesRange = distanceTarget < (fishbonesRange + target.BoundingRadius)
+    local notInOverswaprange = distanceTarget < ((525 + target.BoundingRadius) - Menu.Get("overSwap"))
+    local isFullStack = fishbonesStack == 3
+    local canAoe = GetAoeCount(target, enemies) > (Menu.Get("aoeCount") - 1)
+    if isFishBones then
+        return ((not isInFishBonesRange) or (Menu.Get("powPowFullStack") and isFullStack)) or (Menu.Get("powPowAoe") and canAoe)
+    end
+    return ((not canAoe) and (not isFullStack)) and notInOverswaprange
+end
+function tryQ(enemies)
+    local target = TargetSelector:GetTarget(powPowRange, true)
+    if (not Q:IsReady()) or (not ShouldSwap(
+        ((target and (function() return target end)) or (function() return enemies[1] end))(),
+        enemies
+    )) then
+        return false
+    end
+    return Q:Cast()
+end
+function tryW(hitchance)
+    local target = TargetSelector:GetTarget(wInput.Range, true)
+    if ((not target) or (not W:IsReady())) or (W:GetManaCost() > Player.Mana) then
+        return false
+    end
+    repeat
+        local ____switch40 = Menu.Get("wMode")
+        local ____cond40 = ____switch40 == 1
+        if ____cond40 then
+            do
+                if target.Position:Distance(Player.Position) > powPowRange then
+                    return W:CastOnHitChance(target, hitchance)
+                end
+                break
+            end
+        end
+        ____cond40 = ____cond40 or (____switch40 == 2)
+        if ____cond40 then
+            do
+                if target.Position:Distance(Player.Position) > Menu.Get("wMinRange") then
+                    return W:Cast(target)
+                end
+                break
+            end
+        end
+        ____cond40 = ____cond40 or (____switch40 == 3)
+        if ____cond40 then
+            return W:Cast(target)
+        end
+        do
+            break
+        end
+    until true
+    return false
+end
+function tryE(enemies, hitchance)
+    if (not E.IsReady) or (E:GetManaCost() > Player.Mana) then
+        return false
+    end
+    do
+        local i = 0
+        while i < #enemies do
+            if E:CastOnHitChance(enemies[i + 1], hitchance) then
+                return true
+            end
+            i = i + 1
+        end
+    end
+    return false
+end
+function Combo(enemies)
+    if Menu.Get("eCombo") then
+        if tryE(
+            enemies,
+            Menu.Get("eComboHit")
+        ) then
+            return
+        end
+    end
+    if Menu.Get("qCombo") then
+        if tryQ(enemies) then
+            return
+        end
+    end
+    if Menu.Get("wCombo") and (not Menu.Get("wAuto")) then
+        if tryW(
+            Menu.Get("wComboHit")
+        ) then
+            return
+        end
+    end
+end
+function Harass(enemies)
+    if Menu.Get("eHarass") then
+        if tryE(
+            enemies,
+            Menu.Get("eHarassHit")
+        ) then
+            return
+        end
+    end
+    if Menu.Get("qHarass") then
+        if tryQ(enemies) then
+            return
+        end
+    end
+    if Menu.Get("wHarass") then
+        if tryW(
+            Menu.Get("wHarassHit")
+        ) then
+            return
+        end
+    end
+end
+function HasStatik()
+    for key, item in pairs(Player.Items) do
+        if item and (item.ItemId == 3094) then
+            return true
+        end
+    end
+    return false
+end
+function LastHit()
+    if (Menu.Get("qLastHit") and Q:IsReady()) and (not isFishBones) then
+        Q:Cast()
+    end
+end
+function UpdateStats()
+    local qLevel = Player:GetSpell(SpellSlots.Q).Level
+    fishbonesRange = 525
+    powPowRange = (qLevel * 25) + 600
+    local fishBonesBuff = Player:GetBuff("jinxqramp")
+    if fishBonesBuff then
+        fishbonesStack = fishBonesBuff.Count
+    end
+    local statikBuff = Player:GetBuff("itemstatikshankcharge")
+    if (HasStatik() and statikBuff) and (statikBuff.Count == 100) then
+        fishbonesRange = fishbonesRange + 150
+        powPowRange = powPowRange + 150
+    end
+    isFishBones = not Player:GetBuff("JinxQ")
+end
+function OnTick()
+    if ((((Player.IsDead or Player.IsRecalling) or (not Player.CanCast)) or Player.IsCasting) or Game.IsMinimized()) or Game.IsChatOpen() then
+        return
+    end
+    UpdateStats()
+    local enemies = GetValidNearbyHeroes("enemy")
+    local orbwalkerMode = Orbwalker.GetMode()
+    if #enemies == 0 then
+        return
+    end
+    repeat
+        local ____switch76 = orbwalkerMode
+        local ____cond76 = ____switch76 == "Combo"
+        if ____cond76 then
+            do
+                Combo(enemies)
+                break
+            end
+        end
+        ____cond76 = ____cond76 or (____switch76 == "Harass")
+        if ____cond76 then
+            do
+                Harass(enemies)
+                break
+            end
+        end
+        ____cond76 = ____cond76 or (____switch76 == "Lasthit")
+        if ____cond76 then
+            do
+                LastHit()
+                break
+            end
+        end
+        ____cond76 = ____cond76 or (____switch76 == "Waveclear")
+        if ____cond76 then
+            do
+                break
+            end
+        end
+        do
+            break
+        end
+    until true
+end
+if Player.CharName ~= "Jinx" then
+    return false
+end
+local Core = _G.CoreEx
+ObjectManager = Core.ObjectManager
+Game = Core.Game
+local Enums = Core.Enums
+SpellSlots = Enums.SpellSlots
+local Events = Enums.Events
+local Libs = _G.Libs
+Orbwalker = Libs.Orbwalker
+Menu = Libs.NewMenu
+local SpellLib = Libs.Spell
+TargetSelector = Libs.TargetSelector()
+fishbonesStack = 0
+fishbonesRange = 525
+isFishBones = true
+powPowRange = 600
+local qInput = {Slot = SpellSlots.Q}
+Q = SpellLib.Active(qInput)
+wInput = {Slot = SpellSlots.W, Range = 1500, Speed = 3300, Delay = 0, Radius = 120, Type = "Linear", UseHitbox = true, Collisions = {WindWall = true, Minions = true}}
+W = SpellLib.Skillshot(wInput)
+eInput = {Slot = SpellSlots.E, Range = 1120, Speed = 1850, Delay = 0.9, Radius = 115, Type = "Circular", UseHitbox = false, Collisions = {WindWall = true}}
+E = SpellLib.Skillshot(eInput)
+local rInput = {Slot = SpellSlots.R, Range = math.huge, Speed = 1700, Delay = 0.6, Radius = 280, Type = "Circular", UseHitbox = false, Collisions = {WindWall = true}}
+local R = SpellLib.Skillshot(rInput)
+local events = {{id = Events.OnTick, callback = OnTick}, {id = Events.OnDraw, callback = OnDraw}, {id = Events.OnGapclose, callback = OnGapclose}, {id = Events.OnHeroImmobilized, callback = OnHeroImmobilized}}
+local wModes = {}
+wModes[1] = "Out of AA range"
+wModes[2] = "Min. distance"
+wModes[3] = "Always"
+local hitchances = {}
+hitchances[1] = "Collision"
+hitchances[2] = "Out Of Range"
+hitchances[3] = "Very Low"
+hitchances[4] = "Low"
+hitchances[5] = "Medium"
+hitchances[6] = "High"
+hitchances[7] = "Very High"
+hitchances[8] = "Dashing"
+hitchances[9] = "Immobile"
+local function InitLog()
+    module("PoncheJinx", package.seeall, log.setup)
+    clean.module("PoncheJinx", clean.seeall, log.setup)
+end
+local function InitMenu()
+    local enemies = ObjectManager.Get("enemy", "heroes")
+    local enemiesName = {}
+    local allies = ObjectManager.Get("ally", "heroes")
+    local alliesName = {}
+    local enemiesCount = 0
+    for key, obj in pairs(enemies) do
+        local enemyName = obj.AsHero.CharName
+        if not __TS__ArrayIncludes(enemiesName, enemyName) then
+            __TS__ArrayPush(enemiesName, enemyName)
+        end
+        enemiesCount = enemiesCount + 1
+    end
+    for key, obj in pairs(allies) do
+        do
+            if obj.IsMe then
+                goto __continue7
+            end
+            local allyName = obj.AsHero.CharName
+            if not __TS__ArrayIncludes(alliesName, allyName) then
+                __TS__ArrayPush(alliesName, allyName)
+            end
+        end
+        ::__continue7::
+    end
+    if enemiesCount == 0 then
+        enemiesCount = 1
+    end
+    Menu.RegisterMenu(
+        "PoncheJinx",
+        "PoncheJinx",
+        function()
+            Menu.Text("v1.0", true)
+            Menu.NewTree(
+                "combo",
+                "Combo",
+                function()
+                    Menu.Checkbox("qCombo", "Use [Q]", true)
+                    Menu.Checkbox("wCombo", "Use [W]", true)
+                    Menu.Dropdown("wComboHit", "[W] Hitchance: ", 5, hitchances)
+                    Menu.Checkbox("eCombo", "Use [E]", true)
+                    Menu.Dropdown("eComboHit", "[E] Hitchance: ", 6, hitchances)
+                end
+            )
+            Menu.NewTree(
+                "harass",
+                "Harass",
+                function()
+                    Menu.Checkbox("qHarass", "Use [Q]", true)
+                    Menu.Checkbox("wHarass", "Use [W]", true)
+                    Menu.Dropdown("wHarassHit", "[W] Hitchance: ", 6, hitchances)
+                    Menu.Checkbox("eHarass", "Use [E]", true)
+                    Menu.Dropdown("eHarassHit", "[W] Hitchance: ", 7, hitchances)
+                end
+            )
+            Menu.NewTree(
+                "lastHit",
+                "Last Hit",
+                function()
+                    Menu.Checkbox("qLastHit", "Use [Q]", true)
+                    Menu.Text("Just switch back to Fishbones")
+                end
+            )
+            Menu.NewTree(
+                "qConfig",
+                "[Q] Config",
+                function()
+                    Menu.Checkbox("powPowFullStack", "Switch full stack", false)
+                    Menu.Checkbox("powPowAoe", "Switch for AOE", true)
+                    Menu.Slider("aoeCount", "Min. AOE hitcount ", 2, 1, 3, 1)
+                    Menu.Slider("aoeRadius", "AOE hit radius ", 300, 100, 300, 50)
+                    Menu.Slider("overSwap", "Anti overswap", 60, 0, 150, 10)
+                end
+            )
+            Menu.NewTree(
+                "wConfig",
+                "[W] Config",
+                function()
+                    Menu.Dropdown("wMode", "Cast mode: ", 1, wModes)
+                    Menu.Slider("wMinRange", "Min. distance", 900, 0, wInput.Range, 50)
+                end
+            )
+            Menu.NewTree(
+                "eConfig",
+                "[E] Config",
+                function()
+                    Menu.Checkbox("eOnGapclose", "Auto on gapclose", true)
+                    Menu.NewTree(
+                        "eOnEnemyGap",
+                        "On enemy gapclose:",
+                        function()
+                            for ____, enemyName in ipairs(enemiesName) do
+                                Menu.Checkbox("eOnGap" .. enemyName, enemyName, true)
+                            end
+                        end
+                    )
+                    Menu.Checkbox("eOnCC", "Auto on CC", true)
+                    Menu.Slider("eCCDuration", "Min. CC duration", 0.4, 0, 2, 0.1)
+                end
+            )
+        end
+    )
+end
+local function InitEvents()
+    local eventManager = _G.CoreEx.EventManager
+    do
+        local i = 0
+        while i < #events do
+            eventManager.RegisterCallback(events[i + 1].id, events[i + 1].callback)
+            i = i + 1
+        end
+    end
+end
+local function tryR(enemies)
+    if (not R:IsReady()) or (R:GetManaCost() > Player.Mana) then
+        return false
+    end
+    return false
+end
+OnLoad = function()
+    InitLog()
+    InitMenu()
+    InitEvents()
+    return true
+end
+return ____exports
---
+++ src/PoncheJinx.ts
@@ -0,0 +1,456 @@
+/*
+
+TO_DO:
+
+R
+Wave Clear
+
+*/
+
+if (Player.CharName != 'Jinx') {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return false;
+}
+
+const Core = _G.CoreEx;
+const ObjectManager = Core.ObjectManager;
+const Game = Core.Game;
+const Enums = Core.Enums;
+const SpellSlots = Enums.SpellSlots;
+const Events = Enums.Events;
+
+const Libs = _G.Libs;
+const Orbwalker = Libs.Orbwalker;
+const Menu = Libs.NewMenu;
+const SpellLib = Libs.Spell;
+const TargetSelector = Libs.TargetSelector();
+
+let fishbonesStack = 0;
+let fishbonesRange = 525;
+let isFishBones = true;
+let powPowRange = 600;
+
+const qInput: PredictionInput = {
+  Slot: SpellSlots.Q,
+};
+
+const Q = SpellLib.Active(qInput);
+
+const wInput = {
+  Slot: SpellSlots.W,
+  Range: 1500,
+  Speed: 3300,
+  Delay: 0,
+  Radius: 120,
+  Type: SpellType.Linear,
+  UseHitbox: true,
+  Collisions: { WindWall: true, Minions: true },
+};
+
+const W = SpellLib.Skillshot(wInput);
+
+const eInput = {
+  Slot: SpellSlots.E,
+  Range: 1120,
+  Speed: 1850,
+  Delay: 0.9, // 0.4 + 0.5
+  Radius: 115,
+  Type: SpellType.Circular,
+  UseHitbox: false,
+  Collisions: { WindWall: true },
+};
+
+const E = SpellLib.Skillshot(eInput);
+
+const rInput = {
+  Slot: SpellSlots.R,
+  Range: math.huge,
+  Speed: 1700, // 2200
+  Delay: 0.6,
+  Radius: 280,
+  Type: SpellType.Circular,
+  UseHitbox: false,
+  Collisions: { WindWall: true },
+};
+
+const R = SpellLib.Skillshot(rInput);
+
+const events: EventToRegister[] = [
+  {
+    id: Events.OnTick,
+    callback: OnTick,
+  },
+  {
+    id: Events.OnDraw,
+    callback: OnDraw,
+  },
+  {
+    id: Events.OnGapclose,
+    callback: OnGapclose,
+  },
+  {
+    id: Events.OnHeroImmobilized,
+    callback: OnHeroImmobilized,
+  },
+  /*
+  {
+    id: Events.OnBuffGain,
+    callback: OnBuffGain,
+  },
+  */
+];
+
+const wModes = new LuaTable();
+wModes.set(1, 'Out of AA range');
+wModes.set(2, 'Min. distance');
+wModes.set(3, 'Always');
+
+const hitchances = new LuaTable();
+hitchances.set(1, 'Collision');
+hitchances.set(2, 'Out Of Range');
+hitchances.set(3, 'Very Low');
+hitchances.set(4, 'Low');
+hitchances.set(5, 'Medium');
+hitchances.set(6, 'High');
+hitchances.set(7, 'Very High');
+hitchances.set(8, 'Dashing');
+hitchances.set(9, 'Immobile');
+
+function InitLog(): void {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  module('PoncheJinx', package.seeall, log.setup);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  clean.module('PoncheJinx', clean.seeall, log.setup);
+}
+
+function InitMenu(): void {
+  const enemies = ObjectManager.Get(AllyOrEnemy.Enemy, ObjectType.Heroes);
+  const enemiesName: string[] = [];
+  const allies = ObjectManager.Get(AllyOrEnemy.Ally, ObjectType.Heroes);
+  const alliesName: string[] = [];
+  let enemiesCount = 0;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const [key, obj] of pairs(enemies)) {
+    const enemyName = obj.AsHero.CharName;
+    if (!enemiesName.includes(enemyName)) enemiesName.push(enemyName);
+    enemiesCount++;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const [key, obj] of pairs(allies)) {
+    if (obj.IsMe) continue;
+    const allyName = obj.AsHero.CharName;
+    if (!alliesName.includes(allyName)) alliesName.push(allyName);
+  }
+  if (enemiesCount === 0) enemiesCount = 1;
+  Menu.RegisterMenu('PoncheJinx', 'PoncheJinx', function () {
+    Menu.Text('v1.0', true);
+    Menu.NewTree('combo', 'Combo', function () {
+      Menu.Checkbox('qCombo', 'Use [Q]', true);
+      Menu.Checkbox('wCombo', 'Use [W]', true);
+      Menu.Dropdown('wComboHit', '[W] Hitchance: ', 5, hitchances);
+      Menu.Checkbox('eCombo', 'Use [E]', true);
+      Menu.Dropdown('eComboHit', '[E] Hitchance: ', 6, hitchances);
+      //Menu.Checkbox('rCombo', 'Use [R]', true);
+    });
+    Menu.NewTree('harass', 'Harass', function () {
+      Menu.Checkbox('qHarass', 'Use [Q]', true);
+      Menu.Checkbox('wHarass', 'Use [W]', true);
+      Menu.Dropdown('wHarassHit', '[W] Hitchance: ', 6, hitchances);
+      Menu.Checkbox('eHarass', 'Use [E]', true);
+      Menu.Dropdown('eHarassHit', '[W] Hitchance: ', 7, hitchances);
+    });
+    Menu.NewTree('lastHit', 'Last Hit', function () {
+      Menu.Checkbox('qLastHit', 'Use [Q]', true);
+      Menu.Text('Just switch back to Fishbones');
+    });
+    Menu.NewTree('qConfig', '[Q] Config', function () {
+      Menu.Checkbox('powPowFullStack', 'Switch full stack', false);
+      Menu.Checkbox('powPowAoe', 'Switch for AOE', true);
+      Menu.Slider('aoeCount', 'Min. AOE hitcount ', 2, 1, 3, 1);
+      Menu.Slider('aoeRadius', 'AOE hit radius ', 300, 100, 300, 50);
+      Menu.Slider('overSwap', 'Anti overswap', 60, 0, 150, 10);
+    });
+    Menu.NewTree('wConfig', '[W] Config', function () {
+      Menu.Dropdown('wMode', 'Cast mode: ', 1, wModes);
+      Menu.Slider('wMinRange', 'Min. distance', 900, 0, wInput.Range, 50);
+    });
+    Menu.NewTree('eConfig', '[E] Config', function () {
+      Menu.Checkbox('eOnGapclose', 'Auto on gapclose', true);
+      Menu.NewTree('eOnEnemyGap', 'On enemy gapclose:', function () {
+        for (const enemyName of enemiesName) {
+          Menu.Checkbox('eOnGap' + enemyName, enemyName, true);
+        }
+      });
+      Menu.Checkbox('eOnCC', 'Auto on CC', true);
+      Menu.Slider('eCCDuration', 'Min. CC duration', 0.4, 0, 2, 0.1);
+    });
+    //Menu.NewTree('rConfig', '[R] Config', function () { });
+  });
+}
+
+function InitEvents(): void {
+  const eventManager = _G.CoreEx.EventManager;
+  for (let i = 0; i < events.length; i++) {
+    eventManager.RegisterCallback(events[i].id, events[i].callback);
+  }
+}
+
+function GetValidNearbyHeroes(team: AllyOrEnemy): AIHeroClient[] {
+  const heroes: AIHeroClient[] = [];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const [key, obj] of pairs(
+    ObjectManager.GetNearby(team, ObjectType.Heroes)
+  )) {
+    if (TargetSelector.IsValidTarget(obj)) heroes.push(obj.AsHero);
+  }
+  return heroes;
+}
+
+function OnGapclose(source: AIHeroClient, dash: DashInstance): void {
+  if (
+    Menu.Get('eOnGapclose') &&
+    source.IsHero &&
+    source.IsEnemy &&
+    source.IsValid &&
+    Menu.Get('eOnGap' + source.CharName) &&
+    E.IsReady() &&
+    E.GetManaCost() <= Player.Mana
+  ) {
+    const paths = dash.GetPaths();
+    const endPos = paths[paths.length - 1].EndPos;
+    if (endPos.Distance(Player.Position) <= eInput.Range) {
+      E.Cast(endPos);
+      return;
+    }
+  }
+}
+
+function OnHeroImmobilized(
+  source: AIHeroClient,
+  endTime: number,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  isStasis: boolean
+) {
+  if (
+    Menu.Get('eOnCC') &&
+    source.IsEnemy &&
+    source.IsValid &&
+    E.IsReady() &&
+    E.GetManaCost() <= Player.Mana &&
+    Menu.Get('eCCDuration') <= endTime &&
+    E.CanCast(source)
+  ) {
+    E.Cast(source);
+    return;
+  }
+}
+
+/*
+function OnBuffGain(source: AIHeroClient, buff: BuffInst) {
+  if (
+    Menu.Get('eOnCC') &&
+    source.IsHero &&
+    source.IsEnemy &&
+    source.IsValid &&
+    (buff.IsCC || buff.IsFear || buff.IsRoot) &&
+    E.IsReady() &&
+    E.GetManaCost() <= Player.Mana &&
+    Menu.Get('eCCDuration') <= buff.EndTime &&
+    E.CanCast(source)
+  ) {
+    E.Cast(source);
+    return;
+  }
+}
+*/
+
+function OnDraw(): void { }
+
+function GetAoeCount(target: AIHeroClient, enemies: AIHeroClient[]) {
+  let count = 0;
+  const radius = Menu.Get('aoeRadius');
+  for (let i = 0; i < enemies.length; i++) {
+    if (
+      target.Name != enemies[i].Name &&
+      target.Position.Distance(enemies[i].Position) < radius
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+function ShouldSwap(target: AIHeroClient, enemies: AIHeroClient[]) {
+  const distanceTarget = Player.Position.Distance(target.Position);
+  //const isInPowPowRange = distanceTarget < powPowRange + target.BoundingRadius;
+  const isInFishBonesRange =
+    distanceTarget < fishbonesRange + target.BoundingRadius;
+  const notInOverswaprange =
+    distanceTarget < 525 + target.BoundingRadius - Menu.Get('overSwap');
+  const isFullStack = fishbonesStack === 3;
+  const canAoe = GetAoeCount(target, enemies) > Menu.Get('aoeCount') - 1;
+  if (isFishBones) {
+    return (
+      !isInFishBonesRange /*&& isInPowPowRange*/ ||
+      (Menu.Get('powPowFullStack') && isFullStack) ||
+      (Menu.Get('powPowAoe') && canAoe)
+    );
+  }
+  return !canAoe && !isFullStack && notInOverswaprange;
+}
+
+function tryQ(enemies: AIHeroClient[]): boolean {
+  const target = TargetSelector.GetTarget(powPowRange, true);
+  if (!Q.IsReady() || !ShouldSwap(target ? target : enemies[0], enemies))
+    return false;
+  return Q.Cast();
+}
+
+function tryW(hitchance: keyof Enums_HitChance): boolean {
+  const target = TargetSelector.GetTarget(wInput.Range, true);
+  if (!target || !W.IsReady() || W.GetManaCost() > Player.Mana) return false;
+  switch (Menu.Get('wMode')) {
+    case 1: {
+      if (target.Position.Distance(Player.Position) > powPowRange) {
+        return W.CastOnHitChance(target, hitchance);
+      }
+      break;
+    }
+    case 2: {
+      if (target.Position.Distance(Player.Position) > Menu.Get('wMinRange')) {
+        return W.Cast(target);
+      }
+      break;
+    }
+    case 3:
+      return W.Cast(target);
+    default:
+      break;
+  }
+  return false;
+}
+
+function tryE(
+  enemies: AIHeroClient[],
+  hitchance: keyof Enums_HitChance
+): boolean {
+  if (!E.IsReady || E.GetManaCost() > Player.Mana) return false;
+  for (let i = 0; i < enemies.length; i++) {
+    if (E.CastOnHitChance(enemies[i], hitchance)) return true;
+  }
+  return false;
+}
+
+function tryR(enemies: AIHeroClient[]): boolean {
+  if (!R.IsReady() || R.GetManaCost() > Player.Mana) return false;
+
+  return false;
+}
+
+function Combo(enemies: AIHeroClient[]): void {
+  if (Menu.Get('eCombo')) {
+    if (tryE(enemies, Menu.Get('eComboHit'))) return;
+  }
+  if (Menu.Get('qCombo')) {
+    if (tryQ(enemies)) return;
+  }
+  if (Menu.Get('wCombo') && !Menu.Get('wAuto')) {
+    if (tryW(Menu.Get('wComboHit'))) return;
+  }
+}
+
+function Harass(enemies: AIHeroClient[]): void {
+  if (Menu.Get('eHarass')) {
+    if (tryE(enemies, Menu.Get('eHarassHit'))) return;
+  }
+  if (Menu.Get('qHarass')) {
+    if (tryQ(enemies)) return;
+  }
+  if (Menu.Get('wHarass')) {
+    if (tryW(Menu.Get('wHarassHit'))) return;
+  }
+}
+
+function HasStatik() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  for (const [key, item] of pairs(Player.Items)) {
+    if (item && item.ItemId === 3094) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function LastHit() {
+  if (Menu.Get('qLastHit') && Q.IsReady() && !isFishBones) Q.Cast();
+}
+
+function UpdateStats() {
+  const qLevel = Player.GetSpell(SpellSlots.Q).Level;
+  fishbonesRange = 525;
+  powPowRange = qLevel * 25 + 600;
+  const fishBonesBuff = Player.GetBuff('jinxqramp');
+  if (fishBonesBuff) {
+    fishbonesStack = fishBonesBuff.Count;
+  }
+  const statikBuff = Player.GetBuff('itemstatikshankcharge');
+  if (HasStatik() && statikBuff && statikBuff.Count === 100) {
+    fishbonesRange += 150;
+    powPowRange += 150;
+  }
+  isFishBones = !Player.GetBuff('JinxQ');
+}
+
+function OnTick(): void {
+  if (
+    Player.IsDead ||
+    Player.IsRecalling ||
+    !Player.CanCast ||
+    Player.IsCasting ||
+    Game.IsMinimized() ||
+    Game.IsChatOpen()
+  )
+    return;
+
+  UpdateStats();
+
+  const enemies = GetValidNearbyHeroes(AllyOrEnemy.Enemy);
+
+  const orbwalkerMode = Orbwalker.GetMode();
+
+  if (enemies.length === 0) return;
+
+  switch (orbwalkerMode) {
+    case OrbwalkerMode.Combo: {
+      Combo(enemies);
+      break;
+    }
+    case OrbwalkerMode.Harass: {
+      Harass(enemies);
+      break;
+    }
+    case OrbwalkerMode.Lasthit: {
+      LastHit();
+      break;
+    }
+    case OrbwalkerMode.Waveclear: {
+      //statements;
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+OnLoad = () => {
+  InitLog();
+  InitMenu();
+  InitEvents();
+  return true;
+};
+
+// eslint-disable-next-line prettier/prettier
+export { };
